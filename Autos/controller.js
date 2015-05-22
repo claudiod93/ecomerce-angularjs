23ee24084e474bf3a7fb4323a3c1014aa7705dbf
Claudio Diaz <clau.diazcubillos@gmail.com>
2015-05-22 19:50:48 -0300

    Inicio de versión 1.0

---
+++ Autos/controller.js
@@ -0,0 +1,34 @@
+angular.module("autosModule",[] )
+.controller("autoCtr", function ($scope){
+	$scope.autos=[
+	
+			{marca: "Toyota", modelo: "Yaris", ano: "2010"},
+			{marca: "Kia", modelo: "Cerato", ano: "2013"},
+			{marca: "Chevrolet", modelo: "Spark", ano: "2015"}
+	];
+
+	$scope.save = function(){
+
+	
+		$scope.autos.push({marca: $scope.autoNuevo.marca, modelo: $scope.autoNuevo.modelo, ano: $scope.autoNuevo.ano});
+		$scope.limpiarDatos();
+	
+	};
+
+	$scope.eliminar = function(indice) {
+	var refreshautos = [];
+	for (var i = 0; i < $scope.autos.length; i++) {
+      if (i != indice) {
+         refreshautos.push($scope.autos[i]);
+      }
+	}
+	$scope.autos = refreshautos;
+	};
+
+	$scope.limpiarDatos = function() {
+      $scope.autoNuevo.marca = '';
+      $scope.autoNuevo.modelo = '';
+      $scope.autoNuevo.ano= '';
+  
+    };
+});
